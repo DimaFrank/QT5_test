5d204d5ee4d6e3a52b0cdb58d859435c05ac72ac
Dima.Frank <dimafrank91@gmail.com>
2023-04-11 19:59:27 +0300

    widgets_connection

--- MyApp.py
+++ MyApp.py
@@ -1,40 +1,53 @@
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
-# Only needed for access to command line arguments
 import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from random import choice
 
+window_titles = [
+    'My App',
+    'My App',
+    'Still My App',
+    'Still My App',
+    'What on earth',
+    'What on earth',
+    'This is surprising',
+    'This is surprising',
+    'Something went wrong'
+]
 
-# Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
+        self.n_times_clicked = 0
 
-        self.setFixedSize(QSize(1200, 600))
-        self.setMaximumSize(QSize(1200, 600))
-        self.setMinimumSize(QSize(800, 400))
+        self.setWindowTitle("My App")
+
+        self.button = QPushButton("Press Me!")
+        self.button.clicked.connect(self.the_button_was_clicked)
+
+        self.windowTitleChanged.connect(self.the_window_title_changed)
 
         # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        self.setCentralWidget(self.button)
+
+    def the_button_was_clicked(self):
+        print("Clicked.")
+        new_window_title = choice(window_titles)
+        print("Setting title:  %s" % new_window_title)
+        self.setWindowTitle(new_window_title)
+
+    def the_window_title_changed(self, window_title):
+        print("Window title changed: %s" % window_title)
+
+        if window_title == 'Something went wrong':
+            self.button.setDisabled(True)
 
 
 
-# You need one (and only one) QApplication instance per application.
-# Pass in sys.argv to allow command line arguments for your app.
-# If you know you won't use command line arguments QApplication([]) works too.
+
 app = QApplication(sys.argv)
 
-# Create a Qt widget, which will be our window.
-# window = QWidget()
-# window = QPushButton("Push Me")
-# window = QMainWindow()
 window = MainWindow()
-window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+window.show()
 
-# Start the event loop.
 app.exec()
-
-# Your application won't reach here until you exit and the event
-# loop has stopped.

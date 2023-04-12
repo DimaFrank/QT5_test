00f1cf7324ca743335cdad80b268a90002f22750
Dima.Frank <dimafrank91@gmail.com>
2023-04-12 14:46:35 +0300

    ExcelAnalyzer

--- MyApp.py
+++ MyApp.py
@@ -1,53 +1,105 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
-from random import choice
+import os
+from PyQt5.QtWidgets import (
+    QMainWindow, QApplication,
+    QLabel, QCheckBox, QComboBox, QListWidget, QLineEdit, QFileDialog,
+    QLineEdit, QSpinBox, QDoubleSpinBox, QSlider, QGridLayout, QPushButton, QStyleOptionTitleBar
+)
+from PyQt5.QtCore import Qt
+from pathlib import Path
+import pandas as pd
+import traceback
 
-window_titles = [
-    'My App',
-    'My App',
-    'Still My App',
-    'Still My App',
-    'What on earth',
-    'What on earth',
-    'This is surprising',
-    'This is surprising',
-    'Something went wrong'
-]
 
 class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
 
-        self.n_times_clicked = 0
+    def __init__(self):
+        super(MainWindow, self).__init__()
 
         self.setWindowTitle("My App")
+        self.setMinimumSize(1000, 600)
+        self.setMaximumSize(1200, 600)
 
-        self.button = QPushButton("Press Me!")
-        self.button.clicked.connect(self.the_button_was_clicked)
+        # Creating layout
+        layout = QGridLayout()
+        self.central_widget = QLabel()
+        self.setCentralWidget(self.central_widget)
+        self.central_widget.setLayout(layout)
 
-        self.windowTitleChanged.connect(self.the_window_title_changed)
+        # file selection widgets
+        self.file_label = QLabel('File:')
+        self.file_browse = QPushButton('Browse')
+        self.file_browse.clicked.connect(self.open_file_dialog)
+        self.filename_edit = QLineEdit()
 
-        # Set the central widget of the Window.
-        self.setCentralWidget(self.button)
+        # file selection widgets sizes
+        self.file_browse.setFixedSize(50, 21)
+        self.filename_edit.setFixedSize(400, 20)
 
-    def the_button_was_clicked(self):
-        print("Clicked.")
-        new_window_title = choice(window_titles)
-        print("Setting title:  %s" % new_window_title)
-        self.setWindowTitle(new_window_title)
+        # check boxes
+        self.check_box1 = QCheckBox("Basic Statistics")
+        self.check_box2 = QCheckBox("Correlation Matrix")
+        self.check_box3 = QCheckBox("Graph")
 
-    def the_window_title_changed(self, window_title):
-        print("Window title changed: %s" % window_title)
+        # run button
+        self.run_button = QPushButton('RUN')
+        self.run_button.clicked.connect(self.run_process)
 
-        if window_title == 'Something went wrong':
-            self.button.setDisabled(True)
+        # Adding widgets to layout
+        layout.addWidget(self.file_label, 0, 0)
+        layout.addWidget(self.filename_edit, 0, 1)
+        layout.addWidget(self.file_browse, 0, 2)
+        layout.addWidget(self.check_box1, 1, 0)
+        layout.addWidget(self.check_box2, 1, 1)
+        layout.addWidget(self.check_box3, 2, 0)
+        layout.addWidget(self.run_button, 5, 2)
+
+        # Set layout alignment
+        layout.setAlignment(Qt.AlignCenter | Qt.AlignLeft)
+        # Add empty cells to push filename_edit to the left
+        layout.setColumnStretch(5, 2)
+
+    def open_file_dialog(self):
+        filename, _ = QFileDialog.getOpenFileName(
+            self,
+            "Select a File",
+            str(Path.home()),
+            "All Files (*.csv; *.xlsx);"
+        )
+        if filename:
+            if os.path.isfile(filename) and os.access(filename, os.R_OK):
+                path = Path(filename)
+                self.filename_edit.setText(str(path))
+            else:
+                error_dialog = QLabel("Selected file is not valid or you don't have permission to access it.")
+                error_dialog.exec_()
+
+    def run_process(self):
+        file_ext = self.filename_edit.text().split('.')[-1]
+        if file_ext == 'xlsx':
+            print("This is excel file")
+            try:
+                df = pd.read_excel(self.filename_edit.text())
+            except Exception as e:
+                print("Error reading Excel file:")
+                traceback.print_exc()
+                return
+        elif file_ext == 'csv':
+            print("This is csv file")
+            try:
+                df = pd.read_csv(self.filename_edit.text(), low_memory=False)
+            except Exception as e:
+                print("Error reading CSV file:")
+                traceback.print_exc()
+                return
+
+        print("Shape: {}".format(df.shape))
+        print(df.describe())
 
 
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    app.exec_()
 
-
-app = QApplication(sys.argv)
-
-window = MainWindow()
-window.show()
-
-app.exec()
